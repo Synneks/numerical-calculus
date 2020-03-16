7153c6e545a473d0c29fff0c3f1d46d9d1926808
notCosco <cosminlucaci98@gmail.com>
2020-03-16 21:57:08 +0200

    a ul de la 1

---
+++ lab1/lab1.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 100 linearly spaced numbers
+x = np.linspace(-5,5,1000)
+
+y = (x**2)/(x**2+1)
+
+# setting the axes at the centre
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+
+# plot the function
+plt.plot(x,y, 'r')
+
+# show the plot
+plt.show()
