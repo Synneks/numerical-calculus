b148cead23f0c6adf3a6002b4a51f4645c3c6191
Maris Mihai-Bogdan <maris.mihaibogdan@gmail.com>
2020-03-17 19:44:18 +0200

    prob 1 cu graf si referat

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+import sympy
 
 # 100 linearly spaced numbers
 x = np.linspace(-100, 100, 1000)
@@ -22,5 +24,24 @@ ax.yaxis.set_ticks_position('left')
 plt.plot(x, y, 'r')
 plt.plot(x, [1] * 1000)
 
+taylor = 0
+a = sympy.Symbol('x')
+func = (a ** 2) / (a ** 2 + 1)
+start = 0
+for i in range(3):
+    newTerm = ( (a - start) **i / math.factorial(i) ) * func.subs(a, start)
+    print(newTerm)
+    func = sympy.diff(func, a)
+    taylor += newTerm
+
 # show the plot
+
+#plt.plot(x,taylor,'r')
+
+ytaylor = []
+for val in x:
+    ytaylor.append(taylor.subs(a, val))
+
+plt.plot(x, ytaylor, 'g')
 plt.show()
+
